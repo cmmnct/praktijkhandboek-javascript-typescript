b17112a9c5d2fda0c30f772f0fa28400ca3ccda5
Mark Nijenhuis <mark@cmmnct.nl>
2024-10-29 16:04:53 +0100

    tussentijds

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -4,6 +4,16 @@
     <ion-header>
       <ion-toolbar>
         <ion-title>Memory Game</ion-title>
+        <ion-buttons slot="end">
+     <ion-button @click=" componentState.showUserSettings = true">
+        <ion-icon slot="icon-only" name="person-circle-outline"></ion-icon>
+     </ion-button>
+     <ion-button @click=" componentState.showGridSizeSelector = true">
+        <ion-icon slot="icon-only" name="grid-outline"></ion-icon>
+     </ion-button>
+</ion-buttons>
+
+
       </ion-toolbar>
     </ion-header>
     <ion-content>
@@ -17,11 +27,34 @@
     </ion-content>
     <!-- Modals -->
     <!-- gridSizeSelector -->
+    <ion-modal 
+          :is-open="componentState.showGridSizeSelector" 
+          @didDismiss="componentState.showGridSizeSelector = false">
+      <ion-header>
+        <ion-toolbar>
+          <ion-title>Selecteer Grid Grootte</ion-title>
+          <ion-buttons slot="end">
+            <ion-button 
+               @click="componentState.showGridSizeSelector = false">
+               Sluiten
+            </ion-button>
+          </ion-buttons>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content>
+        <ion-list>
+          <ion-item button @click="handleGridSizeChange(16)">4x4</ion-item>
+          <ion-item button @click="handleGridSizeChange(25)">5x5</ion-item>
+          <ion-item button @click="handleGridSizeChange(36)">6x6</ion-item>
+        </ion-list>
+      </ion-content>
+    </ion-modal>
+
   </ion-page>
 </template>
 
 <script lang="ts" setup>
-import { ref, onMounted} from 'vue';
+import { ref, reactive, onMounted} from 'vue';
 import { useGameStore } from '@/stores/gameStore';
 import CardComponent from '@/components/CardComponent.vue';
 
@@ -33,6 +66,22 @@ const isSettingsOpen = ref(false);
 const handleCardClick = (index:number) => {
   gameStore.handleCardClick(index);
 };
+
+async function handleGridSizeChange(size: number) {
+  componentState.showGridSizeSelector = false;
+  componentState.loading = true;
+  await gameStore.initializeCards(size);
+  componentState.loading = false;
+}
+
+
+const componentState= reactive({
+  loading : true,
+  showGridSizeSelector: false,
+  showUserSettings : false
+
+})
+
 
 onMounted(async () => {
   await gameStore.loadState();
