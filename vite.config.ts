10554b26580e81805f02198a284a82ae9f0ef62c
Mark Nijenhuis <mark@cmmnct.nl>
2024-10-30 10:26:14 +0100

    hoofdstuk 12 PWA

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,50 @@ import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    legacy()
+    legacy(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /\/assets\/img\/.*\.(?:png|jpg|jpeg|svg)$/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'images-cache',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 dagen
+              },
+            },
+          },
+        ],
+      },
+      manifest: {
+        name: 'Memory geheugentrainer',
+        short_name: 'MemoryApp',
+        description: 'Een eenvoudige geheugentrainer met statistieken en notificatie service',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'assets/img/icons/0.75x/memoryldpi.png',
+            sizes: '144x144',
+            type: 'image/png',
+          },
+          {
+            src: '/assets/img/icons/1x/memory.png',
+            sizes: '513x513',
+            type: 'image/png',
+          },
+        ],
+      },
+    })
+
   ],
   resolve: {
     alias: {
